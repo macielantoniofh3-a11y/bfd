58d2b260fed1ace37ccaf498f8597cdef9784c42
macielantoniofh3-a11y <maciel.antoniofh3@gmail.com>
2025-10-21 22:53:52 +0000

    sql

--- aula21-10.py
+++ aula21-10.py
@@ -11,9 +11,42 @@ ctrl + shift + Q
 
 .schema   mostra o esquema de criação da tabela (codigo para criar a tabela)
 
-select indicando a coluna (atributos)
+SELECT indicando a coluna (atributos) COUNT () AS e coluna conta registros
 
-from vai para a tabela
+FROM vai para a tabela
+
+WHERE DEPOIS DO FROM  faz busca (filtro) exata pelo nome ou dados que solicite
+
+GROUP BY agrupar as colunas 
+
+HAVING filtragem só depois do agrupamento group by
+
+ORDER BY  ordenar
+
+SELECT nom MAX((calculo)/2) AS media
+FROM Aluno
+
+
+funções
+COUNT ()
+MAX((CALCULO))
+MIN ((calculo))
+SUM  soma   - SELECT SUM (coluna) 
+AVG media   - SELECT AVG (coluna)
+
+
+SELECT nome
+FROM Aluno
+
+WHERE
+WHERE nome IN
+WHERE nome LIKE "%nome%"
+_ underline quantidade caracteres
+"%li__"
+
+
 
 
 '''
+
+
